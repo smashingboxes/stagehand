195f68380a61daf9824786c6c266e9cf65783113
Reed Law <reedlaw@gmail.com>
2011-10-24 15:08:50 -0400

    middleware

--- lib/stagehand/rack/middleware.rb
+++ lib/stagehand/rack/middleware.rb
@@ -19,7 +19,7 @@ module Stagehand::Rack
                                    )
           # session[:access_token] = response["access_token"]
           # redirect_to root_url
-          [200, {"Content-Type" => "text/html"}, response["access_token"]]
+          [200, {"Content-Type" => "text/html"}, "test"]
         else
           @app.call(env)
         end

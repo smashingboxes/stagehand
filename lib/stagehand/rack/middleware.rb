79061529ea3b2104145e48291838eb517440c416
Reed Law <reedlaw@gmail.com>
2011-10-24 15:26:28 -0400

    params

--- lib/stagehand/rack/middleware.rb
+++ lib/stagehand/rack/middleware.rb
@@ -18,7 +18,7 @@ module Stagehand::Rack
           #                          )
           # session[:access_token] = token_response["access_token"]
           # redirect_to root_url
-          [200, {"Content-Type" => "text/html"}, request.params]
+          [200, {"Content-Type" => "text/html"}, [request.params['code']]
         else
           @app.call(env)
         end

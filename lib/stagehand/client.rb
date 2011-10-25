7f9c450529e8aa3a0699aaee40031581b239bcaf
Reed Law <reedlaw@gmail.com>
2011-10-25 13:31:01 -0400

    session

--- lib/stagehand/client.rb
+++ lib/stagehand/client.rb
@@ -2,6 +2,9 @@ module Stagehand
   # Wrapper for Personas REST API
   class Client
     require 'stagehand/client/oauth'
+    require 'stagehand/client/user'
+
     include Stagehand::Client::OAuth
+    include Stagehand::Client::User
   end
 end

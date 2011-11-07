e011e0765c55a2f0d149b7c66f9a31883adbca9b
Reed Law <reedlaw@gmail.com>
2011-11-07 13:22:39 -0500

    account model

--- lib/stagehand/account.rb
+++ lib/stagehand/account.rb
@@ -1,15 +1,29 @@
-require 'active_model'
-
 module Stagehand
-  class Account < ActiveModel
-    include ActiveModel::Validations
-
-    validates_presence_of :username, :email
-
-    attr_accessor :username, :email
-    def initialize(username, email)
-      @username, @email = username, email
+  class Account
+    def initialize
       @account ||= Stagehand.get_with_access_token("/profile.json", { page: 1 }).parsed_response
     end
+
+    def roles
+      if @account
+        roles = []
+        @account['roles'].each do |r|
+          roles << r['name']
+        end
+        roles
+      else
+        ["guest"]
+      end
+    end
+
+    def is?(role)
+      roles.include?(role.to_s)
+    end
+
+    def method_missing(method, *params)
+      method = method.to_s
+      return @account[method] if @account.keys.include?(method)  
+      super  
+    end  
   end
 end

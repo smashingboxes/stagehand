3a1596a4dcef900454155405f37e10c1d66bee83
Reed Law <reed@smashingboxes.com>
2012-03-06 15:57:53 -0500

    add method for accounts

--- lib/stagehand/client/account.rb
+++ lib/stagehand/client/account.rb
@@ -17,6 +17,20 @@ module Stagehand
         get_with_access_token("/people/find_by_email/#{email}").body
       end
 
+      # Public: Finds accounts by role
+      #
+      # role   - Role to search by
+      #
+      # Examples
+      #
+      #   find_by_role("admin")
+      #   # => "{\"id\":\"4e691653fc595d0001000002\",\"account_id\"
+      #      :\"4ede1caf9a811123ca000001\"}"
+      #
+      # Returns accounts JSON object
+      def find_by_role(role)
+        get_with_access_token("/accounts/find_by_role/#{ERB::Util.url_encode(role)}")
+      end
     end
   end
 end
